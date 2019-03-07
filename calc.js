e53af14c89ee18071b12b3dcf2e797eb75765a30
guoshibo <1137603852@qq.com>
2019-03-07 08:29:00 +0800

    mocha-fe

---
+++ calc.js
@@ -0,0 +1,12 @@
+function rectangle() {
+  return {
+    'perimeter' : function(w, h) {
+            return 2 * ( w + h);
+              
+    },
+    'area': function(w, h) {
+            return w * h;
+    }  
+  };
+
+}
