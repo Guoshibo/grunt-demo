08f0396a874d23c0f93e546306804391a0d235cf
guoshibo <1137603852@qq.com>
2019-03-12 15:08:21 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,19 @@
-
 module.exports = function (grunt) {
   grunt.initConfig({
-    mocha: {
-      test: {
-        src: ['test/index.html'],   
-      },
-      options: {
-        run: true,
-        reporter: 'Dot'  
-      }   
-    }          
+        imagemin: {                               
+          files: {
+                    expand: true,
+          src: ['images/*.{png,jpg,gif}'],
+                                 dest: 'dist/'
+                                       
+          }
+              }
+                
   });
 
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.registerTask('default', ['mocha']);
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+      grunt.registerTask('default', ['imagemin']);    
 
 };
+
